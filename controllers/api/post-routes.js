a96dec4fe082b678e200dd03827fb5855778453b
Aaron Neslin <aneslin125@gmail.com>
2022-02-22 08:12:28 -0500

    new hd coming!

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
-const sequelize = require('../../config/connection')
+const { Post, User, Vote, Comment } = require("../../models");
+
 
 router.get("/", (req, res) => {
   console.log("=====================");
@@ -11,6 +11,14 @@ router.get("/", (req, res) => {
       {
         model: User,
         attributes: ["username"],
+      },
+      {
+        model:Comment,
+        attributes:['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include:{
+          model: User,
+          attributes:['username']
+        }
       }
     ]
   })

cc6ab43bd128b35b5c7cdae096fe4b71e001ed0f
Aaron Neslin <aneslin125@gmail.com>
2022-02-22 21:45:38 -0500

    added seeds file

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -125,7 +125,7 @@ router.post("/login", (req, res) => {
       res.status(400).json({ message: "Incorrect password" });
       return;
     }
-    req.sesson.save(() => {
+    req.session.save(() => {
       req.session.user_id = dbUserData.id;
       req.session.username = dbUsernameData.username;
       req.session.loggedIn = true;
